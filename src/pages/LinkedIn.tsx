0797e8ca52fa2df713c0d843ca5600ed7c477013
Matheus Chagas Dias <matheuscd10@gmail.com>
2026-03-26 16:25:49 -0300

    workshop banner removal

--- src/pages/LinkedIn.tsx
+++ src/pages/LinkedIn.tsx
@@ -21,10 +21,6 @@ import imgDiagnosticoAncor1 from "figma:asset/c749228b0ff391c90f3ff81ae48965e50b
 // Imagem Ancor Academy
 import imgAncorAcademy from "figma:asset/5b5b0054dbe9ee02d2a7ff1e8432fbd847bfdc8a.png";
 
-// Imagens Workshop - Do Topo ao Próximo Ciclo (2 imagens)
-import imgWorkshop from "figma:asset/ba5819ef27eb4672cb37709313675fe72661e7ad.png";
-import imgWorkshop1 from "figma:asset/f6d349d24dd0e898a4052f62059589f943602f9c.png";
-
 // Imagens O Curso (2 imagens)
 import imgCurso from "figma:asset/5b2e4b3c5e0262156e92b83553c429a3951e9941.png";
 import imgCurso1 from "figma:asset/a1304d1d2901752557556b3cb7182096fac86349.png";
@@ -114,19 +110,6 @@ export default function LinkedIn() {
             <img src={imgPalestras} alt="" className="absolute inset-0 w-full h-full object-cover rounded-[32px]" />
             <img src={imgPalestras1} alt="" className="absolute inset-0 w-full h-full object-cover rounded-[32px]" />
             <img src={imgPalestras2} alt="Palestras e Programas In Company" className="absolute inset-0 w-full h-full object-cover rounded-[32px]" />
-          </div>
-        </a>
-
-        {/* 6. Banner: Workshop Do Topo ao Próximo Ciclo */}
-        <a
-          href="https://workshop.ancorcarreira.com.br/"
-          className="block w-full rounded-[32px] shadow-[0px_1px_3px_0px_rgba(0,0,0,0.1),0px_1px_2px_0px_rgba(0,0,0,0.1)] transition-transform hover:scale-[1.02] hover:shadow-md overflow-hidden"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="relative w-full" style={{ paddingBottom: '37.12%' }}>
-            <img src={imgWorkshop} alt="" className="absolute inset-0 w-full h-full object-cover rounded-[32px]" />
-            <img src={imgWorkshop1} alt="Workshop Do Topo ao Próximo Ciclo" className="absolute inset-0 w-full h-full object-cover rounded-[32px]" />
           </div>
         </a>
 
